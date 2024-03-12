5460aa5be7ca6bf180b22ef453a6d955fd525942
kaif <asifkaif21@gmail.com>
2024-03-12 15:30:47 +0530

    table set

--- src/main/java/com/kaif/dockersringboot/controllers/Dockertesting.java
+++ src/main/java/com/kaif/dockersringboot/controllers/Dockertesting.java
@@ -1,26 +1,32 @@
 package com.kaif.dockersringboot.controllers;
 
-import org.apache.catalina.connector.Request;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kaif.dockersringboot.entities.Device;
+import com.kaif.dockersringboot.repo.DeviceRepo;
 
 @RestController
 public class Dockertesting {
+
+    @Autowired
+    private DeviceRepo service;
 
     @GetMapping("/")
     public String testMethod() {
         return "get sucess";
     }
 
-    @PostMapping("/") 
+    @PostMapping("/")
     public String postData(@RequestBody Device data) {
         System.out.println("************************************");
         System.out.println(data);
+        service.save(data);
         System.out.println("************************************");
+
         return "post success";
     }
 
