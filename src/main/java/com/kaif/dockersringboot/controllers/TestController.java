01f82e2f5f2efd1a2e26013862fb1474db9b097e
kaif <asifkaif21@gmail.com>
2024-03-16 14:03:50 +0530

    send endpoint chnages

--- src/main/java/com/kaif/dockersringboot/controllers/TestController.java
+++ src/main/java/com/kaif/dockersringboot/controllers/TestController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kaif.dockersringboot.entities.Customer;
+
 @RestController
 public class TestController {
 
@@ -22,16 +24,17 @@ public class TestController {
         return "get /send success";
     }
 
-    @PostMapping("/send")
-    public String sendData(@RequestBody Map<String, Object> payload) {
-        System.out.println("**************ddddddddd*************************");
-        for (Map.Entry<String, Object> entry : payload.entrySet()) {
-            String key = entry.getKey();
-            Object value = entry.getValue();
-            System.out.println("Key: " + key + ", Value: " + value);
-        }
-        System.out.println("**************ddddddddd*************************");
-        return "post /send success";
-    }
+    
+    // @PostMapping("/send")
+    // public String sendData(@RequestBody Map<String, Object> payload) {
+    //     System.out.println("**************ddddddddd*************************");
+    //     for (Map.Entry<String, Object> entry : payload.entrySet()) {
+    //         String key = entry.getKey();
+    //         Object value = entry.getValue();
+    //         System.out.println("Key: " + key + ", Value: " + value);
+    //     }
+    //     System.out.println("**************ddddddddd*************************");
+    //     return "post /send success";
+    // }
 
 }
