8bcee740b11d18f9297397dc4432f33d5dd49df2
kaif <asifkaif21@gmail.com>
2024-03-18 11:10:21 +0530

    saving to postgress and mysql database

--- src/main/java/com/kaif/dockersringboot/controllers/TestController.java
+++ src/main/java/com/kaif/dockersringboot/controllers/TestController.java
@@ -1,23 +1,38 @@
 package com.kaif.dockersringboot.controllers;
 
 import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kaif.dockersringboot.entities.Device;
+import com.kaif.dockersringboot.repos.DeviceRepo;
+
 @RestController
 public class TestController {
 
+    @Autowired
+     private DeviceRepo deviceRepo;
+
     @PostMapping("/send")
-    public String sendData(@RequestBody Map<String, Object> payload) {
-        System.out.println("**************ddddddddd*************************");
-        for (Map.Entry<String, Object> entry : payload.entrySet()) {
-            String key = entry.getKey();
-            Object value = entry.getValue();
-            System.out.println("Key: " + key + ", Value: " + value);
-        }
-        System.out.println("**************ddddddddd*************************");
+    public String sendData(@RequestBody Device payload) {
+
+        deviceRepo.save(payload);
+       
         return "post /send success";
     }
+    // @PostMapping("/send")
+    // public String sendData(@RequestBody Map<String, Object> payload) {
+    //     System.out.println("**************ddddddddd*************************");
+    //     for (Map.Entry<String, Object> entry : payload.entrySet()) {
+    //         String key = entry.getKey();
+    //         Object value = entry.getValue();
+    //         System.out.println("Key: " + key + ", Value: " + value);
+    //     }
+    //     System.out.println("**************ddddddddd*************************");
+    //     return "post /send success";
+    // }
 
 }
