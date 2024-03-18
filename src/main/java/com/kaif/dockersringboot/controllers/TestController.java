a0f31952003ee79acdf9fdb64269666a9c8a7432
kaif <asifkaif21@gmail.com>
2024-03-18 14:33:35 +0530

    a

--- src/main/java/com/kaif/dockersringboot/controllers/TestController.java
+++ src/main/java/com/kaif/dockersringboot/controllers/TestController.java
@@ -1,7 +1,5 @@
 package com.kaif.dockersringboot.controllers;
 
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,25 +12,25 @@ import com.kaif.dockersringboot.repos.DeviceRepo;
 public class TestController {
 
     @Autowired
-     private DeviceRepo deviceRepo;
+    private DeviceRepo deviceRepo;
 
     @PostMapping("/send")
     public String sendData(@RequestBody Device payload) {
 
         deviceRepo.save(payload);
-       
+
         return "post /send success";
     }
     // @PostMapping("/send")
     // public String sendData(@RequestBody Map<String, Object> payload) {
-    //     System.out.println("**************ddddddddd*************************");
-    //     for (Map.Entry<String, Object> entry : payload.entrySet()) {
-    //         String key = entry.getKey();
-    //         Object value = entry.getValue();
-    //         System.out.println("Key: " + key + ", Value: " + value);
-    //     }
-    //     System.out.println("**************ddddddddd*************************");
-    //     return "post /send success";
+    // System.out.println("**************ddddddddd*************************");
+    // for (Map.Entry<String, Object> entry : payload.entrySet()) {
+    // String key = entry.getKey();
+    // Object value = entry.getValue();
+    // System.out.println("Key: " + key + ", Value: " + value);
+    // }
+    // System.out.println("**************ddddddddd*************************");
+    // return "post /send success";
     // }
 
 }
