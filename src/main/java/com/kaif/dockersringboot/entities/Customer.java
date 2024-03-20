2d8cf95452ae8e91268200da3e47736d28b3d102
kaif <asifkaif21@gmail.com>
2024-03-20 22:08:13 +0530

    s

--- src/main/java/com/kaif/dockersringboot/entities/Customer.java
+++ src/main/java/com/kaif/dockersringboot/entities/Customer.java
@@ -1,10 +1,10 @@
 package com.kaif.dockersringboot.entities;
 
-import jakarta.annotation.Generated;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+// import jakarta.annotation.Generated;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,11 +12,11 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
+// @Entity
 public class Customer {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // @Id
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
--- src/main/java/com/kaif/dockersringboot/subscriber/CustomerSubscribe.java
+++ src/main/java/com/kaif/dockersringboot/subscriber/CustomerSubscribe.java
@@ -3,26 +3,26 @@ package com.kaif.dockersringboot.subscriber;
 
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import com.kaif.dockersringboot.entities.Customer;
-import com.kaif.dockersringboot.repos.CustomerRepo;
+// import com.kaif.dockersringboot.repos.CustomerRepo;
 
 
 @Service
 public class CustomerSubscribe {
 
-    @Autowired
-    private CustomerRepo customerRepo;
+    // @Autowired
+    // private CustomerRepo customerRepo;
 
     Logger log = LoggerFactory.getLogger(CustomerSubscribe.class);
 
     @KafkaListener(topics = "customer-topic", groupId = "customergroup")
     public void listen(Customer customer) {
         log.info("Subscribed customer json: {}", customer);
-        customerRepo.save(customer);
+        // customerRepo.save(customer);
     }
 
 
