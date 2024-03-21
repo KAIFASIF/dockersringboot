e265fb897f1ec5cf37886a2e8e6ff704b2992918
kaif <asifkaif21@gmail.com>
2024-03-21 16:36:18 +0530

    only kafka

--- src/main/java/com/kaif/dockersringboot/entities/Device.java
+++ src/main/java/com/kaif/dockersringboot/entities/Device.java
@@ -1,9 +1,9 @@
 package com.kaif.dockersringboot.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,12 +11,12 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
+// @Entity
 public class Device {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    // @Id
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // private Long id;
 
     private Long time;
     private String msg;
