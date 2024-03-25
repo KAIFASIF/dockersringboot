5e9c2c5b5ac9f66fa19e837eee2005dc497a7ec3
kaif <asifkaif21@gmail.com>
2024-03-25 15:28:58 +0530

    cors

--- src/main/java/com/kaif/dockersringboot/DockersringbootApplication.java
+++ src/main/java/com/kaif/dockersringboot/DockersringbootApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
@@ -26,6 +28,22 @@ public class DockersringbootApplication {
         }
         return FirebaseMessaging.getInstance(FirebaseApp.getInstance("my-app"));
     }
+
+
+    
+    @Bean		
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedHeaders("*")
+						.allowedMethods("*")
+						.exposedHeaders("*");
+			}	
+		};
+	}
 
     public static void main(String[] args) {
         SpringApplication.run(DockersringbootApplication.class, args);
